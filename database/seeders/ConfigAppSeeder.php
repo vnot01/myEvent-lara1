5f3e53af1540a3ba8b5137a03a845978a8482cee
ngajar-feri <feri.febria@gmail.com>
2023-07-06 15:30:28 +0700

    basis data

--- database/seeders/ConfigAppSeeder.php
+++ database/seeders/ConfigAppSeeder.php
@@ -16,14 +16,11 @@ class ConfigAppSeeder extends Seeder
         DB::table('config_apps')->insert([
             [
             'app_name'=>config('app.name'),
-            'description'=>'<p class="text-white">Festava Live is free CSS template provided by TemplateMo website. This
-            layout is built on Bootstrap v5.2.2 CSS library. You are free to use this template for
-            your commercial website.</p>
-            <h6 class="text-white mt-4">Once in Lifetime Experience</h6>
-            <p class="text-white">You are not allowed to redistribute the template ZIP file on any other
-                website without a permission.</p>
-            <h6 class="text-white mt-4">Whole Night Party</h6>
-            <p class="text-white">Please tell your friends about our website. Thank you.</p>',
+            'description'=>'Festava Live is free CSS template provided by TemplateMo website. This layout is built on Bootstrap v5.2.2 CSS library. You are free to use this template for your commercial website.
+            Once in Lifetime Experience.
+            You are not allowed to redistribute the template ZIP file on any other website without a permission.
+            Whole Night Party
+            Please tell your friends about our website. Thank you.',
             'is_active'=>true,
             ],
         ]);
--- database/seeders/EventsSeeder.php
+++ database/seeders/EventsSeeder.php
@@ -18,6 +18,9 @@ class EventsSeeder extends Seeder
                 'tittle'=>'Event1',
                 'venue_id'=>'1',
                 'slug'=>'event1',
+                'thumbnail'=>'images/events/thumbnail/mZyCKGo1lu3SFCSOQmxB6OGsGynk5TvTrrjXPnge.jpg',
+                'poster'=>'images/events/poster/k7QyrhQflKXKCfENXDdz8i4QB4PwIeX8obJMzhXE.jpg',
+                'venue_id'=>'1',
                 'video_link'=>'eMNRoijZPXgBVmmufYAf1vDAM2ArQCSc90SolZz8.mp4',
                 'is_featured'=>true
             ],
@@ -25,6 +28,9 @@ class EventsSeeder extends Seeder
                 'tittle'=>'Event2',
                 'venue_id'=>'1',
                 'slug'=>'event2',
+                'thumbnail'=>'images/events/thumbnail/C7RmMhD0LVO9F4lAwcMbsPNTn6ur2aTrANYKvvgG.jpg',
+                'poster'=>'images/events/poster/cEO07w4l7b9YAj5brC9sTXCmzE89SpT8w1mXgwk4.png',
+                'venue_id'=>'1',
                 'video_link'=>null,
                 'is_featured'=>false
             ],
@@ -32,6 +38,9 @@ class EventsSeeder extends Seeder
                 'tittle'=>'Event3',
                 'venue_id'=>'2',
                 'slug'=>'event3',
+                'thumbnail'=>'images/events/thumbnail/NmbDRCc5EpIjv3Ak49ZvymlQGywtGFvuX9S0hwiP.jpg',
+                'poster'=>'images/events/poster/KFJ3tFr1FtbYTkv2vH8zL2CPkEBVhPEhL6kdAKCH.jpg',
+                'venue_id'=>'3',
                 'video_link'=>null,
                 'is_featured'=>false
             ]
